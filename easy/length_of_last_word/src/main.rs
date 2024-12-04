7466608cb00b09606cb2428879a34571efa22ee5
Paulo Victor Fernandes Sousa <paulo@MacBook-Pro-de-Paulo.local>
2024-12-04 20:19:10 -0300

    length_of_last_word

---
+++ easy/length_of_last_word/src/main.rs
@@ -0,0 +1,15 @@
+pub struct Solution;
+impl Solution {
+    pub fn length_of_last_word(s: String) -> i32 {
+        println!("s: {}", s);
+
+        2 as i32
+        
+    }
+}
+
+
+fn main() {
+    
+    let a = Solution::length_of_last_word("Hello, world!");
+}
