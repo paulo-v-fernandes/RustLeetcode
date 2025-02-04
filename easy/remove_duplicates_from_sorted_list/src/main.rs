39051276652d5574a119b102a69383bbb4a1d533
Paulo Victor Fernandes Sousa <paulo@MacBook-Pro-de-Paulo.local>
2025-02-04 18:05:17 -0300

    feat/remove_duplicates_from_sorte_list

--- easy/remove_duplicates_from_sorted_list/src/main.rs
+++ easy/remove_duplicates_from_sorted_list/src/main.rs
@@ -1,22 +1,3 @@
-// Definition for singly-linked list.
-#[derive(PartialEq, Eq, Clone, Debug)]
-pub struct ListNode {
-    pub val: i32,
-    pub next: Option<Box<ListNode>>,
-}
-
-impl ListNode {
-    #[inline]
-    pub fn new(val: i32) -> Self {
-        ListNode {
-            next: None,
-            val,
-        }
-    }
-}
-
-pub struct Solution;
-
 impl Solution {
     pub fn delete_duplicates(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
         let mut current = head.as_mut();
@@ -34,16 +15,4 @@ impl Solution {
 
         head
     }
-}
-
-fn main() {
-    let node3 = Box::new(ListNode::new(3));
-    let node3_dup = Box::new(ListNode { val: 3, next: Some(node3) });
-    let node2 = Box::new(ListNode { val: 2, next: Some(node3_dup) });
-    let node1_dup = Box::new(ListNode { val: 1, next: Some(node2) });
-    let head = Some(Box::new(ListNode { val: 1, next: Some(node1_dup) }));
-
-    let result = Solution::delete_duplicates(head);
-
-    println!("{:?}", result);
 }
