8550d7f776c55831fe7a526313adf0a0561c3bd9
Paulo Victor Fernandes Sousa <paulo@MacBook-Pro-de-Paulo.local>
2026-01-02 18:57:13 -0300

    feat(169): Majoritary with boyer-moore

--- easy/majority_element/src/main.rs
+++ easy/majority_element/src/main.rs
@@ -2,14 +2,27 @@ pub struct Solution;
 
 impl Solution {
     pub fn majority_element(nums: Vec<i32>) -> i32 {
-        let n = lenght(nums);
-        nums[0]        
+        let mut count = 0;
+        let mut majoritary = 0;
+
+        for num in nums{
+            if count == 0 {
+                majoritary = num;
+            }
+            if majoritary != num{
+                count -= 1 ;
+            }else{
+                count +=1;
+            }
+            }
+            majoritary
+               
     }
 }
 
 
 fn main() {
-    let nums = vec![1,2,3,4,5];
+    let nums = vec![3,2,3];
     let result = Solution::majority_element(nums);
     println!("Response: {}", result);
 }
