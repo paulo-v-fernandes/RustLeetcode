fe1596093ed60f1cd54b56e17202fb94d425851d
paulo-v-fernandes <p.victorsousapro@gmail.com>
2026-01-02 17:14:27 -0300

    base/palin

---
+++ easy/majority_element/src/main.rs
@@ -0,0 +1,15 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn majority_element(nums: Vec<i32>) -> i32 {
+        let n = lenght(nums);
+        nums[0]        
+    }
+}
+
+
+fn main() {
+    let nums = vec![1,2,3,4,5];
+    let result = Solution::majority_element(nums);
+    println!("Response: {}", result);
+}
